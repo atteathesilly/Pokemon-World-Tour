a09a8e72fe53851450e3792a8de37c6462d502fc
ZinniaGameDev <xeuorux@gmail.com>
2025-04-26 09:54:31 -0700

    BELLIGERENT and similar abilities no longer trigger from Eclipse

--- Plugins/Tectonic Battle/Abilities/Ability Event Handlers/Other Triggers/AbilitiesOnStatLoss.rb
+++ Plugins/Tectonic Battle/Abilities/Ability Event Handlers/Other Triggers/AbilitiesOnStatLoss.rb
@@ -1,27 +1,27 @@
 BattleHandlers::AbilityOnStatLoss.add(:COMPETITIVE,
   proc { |ability, battler, _stat, user|
-      next if user && !user.opposes?(battler)
+      next unless user&.opposes?(battler)
       battler.tryRaiseStat(:SPECIAL_ATTACK, battler, ability: ability, increment: 4)
   }
 )
 
 BattleHandlers::AbilityOnStatLoss.add(:DEFIANT,
   proc { |ability, battler, _stat, user|
-      next if user && !user.opposes?(battler)
+      next unless user&.opposes?(battler)
       battler.tryRaiseStat(:ATTACK, battler, ability: ability, increment: 4)
   }
 )
 
 BattleHandlers::AbilityOnStatLoss.add(:BELLIGERENT,
   proc { |ability, battler, _stat, user|
-      next if user && !user.opposes?(battler)
+      next unless user&.opposes?(battler)
       battler.pbRaiseMultipleStatSteps([:ATTACK, 3, :SPECIAL_ATTACK, 3], battler, ability: ability)
   }
 )
 
 BattleHandlers::AbilityOnStatLoss.add(:IMPERIOUS,
   proc { |ability, battler, _stat, user|
-      next if user && !user.opposes?(battler)
+      next unless user&.opposes?(battler)
       battler.tryRaiseStat(:SPEED, battler, ability: ability, increment: 4)
   }
 )
