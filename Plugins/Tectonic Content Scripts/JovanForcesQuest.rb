c6504a7e8eacf1b0f67da8f6df8b8e0ba5a28d6f
ZinniaGameDev <xeuorux@gmail.com>
2025-06-19 14:20:11 -0700

    More FoN quest work

--- Plugins/Tectonic Content Scripts/JovanForcesQuest.rb
+++ Plugins/Tectonic Content Scripts/JovanForcesQuest.rb
@@ -7,8 +7,12 @@ def jovanFliesAway
   }
 end
 
+FORCES_QUEST_STAGE = 45
+JOVAN_PHONECALL_2_GLOBAL = 236
+
 # To be used on the electric fences keeping them imprisoned
 def forceFliesAway(eventID,species,globalSwitchID)
+  pbWait(50)
   showExclamation(eventID)
   pbWait(30)
   blackFadeOutIn {
@@ -18,4 +22,20 @@ def forceFliesAway(eventID,species,globalSwitchID)
     setGlobalSwitch(globalSwitchID)
   }
   setMySwitch('B')
+  incrementGlobalVar(FORCES_QUEST_STAGE)
+
+  if getGlobalVariable(FORCES_QUEST_STAGE) >= 5
+      pbWait(80)
+      setGlobalSwitch(JOVAN_PHONECALL_2_GLOBAL)
+  end
+end
+
+def jovanFliesPlayerToBarren(incrementVar = true)
+  Pokemon.play_cry(:TROPIUS)
+  pbWait(40)
+  blackFadeOutIn(40) {
+    pbSEPlay("Fly")
+    incrementGlobalVar(FORCES_QUEST_STAGE) if incrementVar
+    transferPlayerToEvent(9,Right,447)
+  }
 end
