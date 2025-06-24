c0d6bc09fb0f3963e121fc6edb71685af874a5a0
AlphaKretin <deewhydeeecks@gmail.com>
2025-06-24 11:47:51 +0930

    Fix map ID for sus package quest switches

--- Plugins/Tectonic Save Conversions/3.3/3.3QuestLog.rb
+++ Plugins/Tectonic Save Conversions/3.3/3.3QuestLog.rb
@@ -127,10 +127,10 @@ SaveData.register_conversion(:quest_log_3_3_0) do
       questLog.advanceQuest(:QUEST_POKEMON_MASTER) if selfSwitches[[18,2, 'B']]
       questLog.completeQuest(:QUEST_POKEMON_MASTER, skipAlert: true) if selfSwitches[[17,4, 'C']]
       
-      questLog.advanceQuest(:QUEST_SUS_PACKAGE) if selfSwitches[[59,18, 'A']]
+      questLog.advanceQuest(:QUEST_SUS_PACKAGE) if selfSwitches[[159,18, 'A']]
       if itemBag.pbHasItem?(:WEIRDPACKAGE)
         questLog.advanceQuest(:QUEST_SUS_PACKAGE)
-        if !selfSwitches[[59,18, 'A']]
+        if !selfSwitches[[159,18, 'A']]
             # skip to second stage if obtained the package first
             questLog.advanceQuest(:QUEST_SUS_PACKAGE)           
         end
