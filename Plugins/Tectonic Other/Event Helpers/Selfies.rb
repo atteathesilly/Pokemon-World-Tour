574a7d95424b2db4f7780aed5b7f3d939807ca50
ZinniaGameDev <xeuorux@gmail.com>
2025-06-28 10:53:48 -0700

    Selfies can be captioned via player input

--- Plugins/Tectonic Other/Event Helpers/Selfies.rb
+++ Plugins/Tectonic Other/Event Helpers/Selfies.rb
@@ -1,4 +1,5 @@
 def takeSelfie
+    caption = ""
     blackFadeOutIn {
       # move follower mon
       $PokemonTemp.dependentEvents.refresh_sprite(true)
@@ -7,9 +8,34 @@ def takeSelfie
 
       get_player.turn_down
       followerEvent.turn_down
+
+      # Set the text
+      caption = pbEnterText(_INTL("Enter caption."),0,50,"",0,nil,true)
     }
-    pbWait(20)
+    pbWait(10)
+
+    # Set up text graphics elements
+    base   = MessageConfig.pbDefaultTextMainColor
+    shadow = MessageConfig.pbDefaultTextShadowColor
+    viewport = Viewport.new(0,0,Graphics.width,Graphics.height)
+    viewport.z = 99999
+    overlay = BitmapSprite.new(Graphics.width, Graphics.height, viewport)
+    overlay.bitmap.font.size = 32
+    
+    # Draw the text
+    pbDrawTextPositions(overlay.bitmap,[
+      [caption,Graphics.width / 2, Graphics.height / 2 + 40, 2, base, shadow, true]
+    ])
+
+    pbWait(30)
+
+    # Take the screenshot
     timeLabel = Time.now.strftime("[%Y-%m-%d] %H_%M_%S.%L")
     pbScreenCapture(_INTL("Selfie_{1}_{2}",$game_map.name,timeLabel))
-    pbWait(20)
+
+    pbWait(80)
+
+    # Dispose of elements
+    overlay.dispose
+    viewport.dispose
 end
