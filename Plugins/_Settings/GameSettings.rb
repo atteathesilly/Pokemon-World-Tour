64c21337d064ff54956928f8b34da380f355a273
ZinniaGameDev <xeuorux@gmail.com>
2025-06-29 13:18:50 -0700

    Revert "Update GAME_VERSION to 3.3.1"
    This reverts commit 6dd1d52a5376afa53fa9c9bfe85b9a7995ef2366.

--- Plugins/_Settings/GameSettings.rb
+++ Plugins/_Settings/GameSettings.rb
@@ -1,6 +1,6 @@
 module Settings
     # The version of your game. It has to adhere to the MAJOR.MINOR.PATCH format.
-    GAME_VERSION = "3.3.1"
+    GAME_VERSION = "3.3.0"
     DEV_VERSION  = false
   
     #=============================================================================
