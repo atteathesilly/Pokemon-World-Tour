d39f9687f957facec3506b842f2f1aaa9592cfd9
ZinniaGameDev <xeuorux@gmail.com>
2025-07-13 15:03:52 -0700

    Fix INCOGNITO

--- Plugins/Tectonic Battle/Effects/EffectDefinitions/BattlerEffects.rb
+++ Plugins/Tectonic Battle/Effects/EffectDefinitions/BattlerEffects.rb
@@ -512,7 +512,7 @@ GameData::BattleEffect.register_effect(:Battler, {
             if idxLastParty >= 0 && idxLastParty != battler.pokemonIndex
                 toDisguiseAs = battle.pbParty(battler.index)[idxLastParty]
                 battler.applyEffect(:Illusion, toDisguiseAs)
-                battler.resetAbilities if battler.hasActiveAbility?(:INCOGNITO)
+                battler.addIllusionAbilities if battler.hasActiveAbility?(:INCOGNITO)
             end
         end
 
