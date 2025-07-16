03a50a949aea38d312ac853334769e008546111c
RiptideHub <66923704+RiptideHub@users.noreply.github.com>
2025-07-16 12:31:05 -0700

    Revert "Added intended curse name"
    This reverts commit f8530a0e65a32f87560c9a54f3384945ae132fa9.

--- Plugins/Tectonic Battle/AI/Trainers AI/Curses/Recycling.rb
+++ Plugins/Tectonic Battle/AI/Trainers AI/Curses/Recycling.rb
@@ -1,7 +1,7 @@
 PokeBattle_Battle::BattleStartApplyCurse.add(:CURSE_RECYCLING,
     proc { |curse_policy, battle, curses_array|
         battle.amuletActivates(
-            _INTL("-to a River of Eight Million Grains of Sand that Wears Down the Highest Peak in-"),
+            _INTL("Eternity in our clutches. Ouroboros ever-yearning."),
             _INTL("Enemy Pokemon recycle consumed items at end of turn. Stolen items turn to dust."),
         )
         curses_array.push(curse_policy)
