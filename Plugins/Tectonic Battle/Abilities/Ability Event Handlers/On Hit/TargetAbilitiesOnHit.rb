916cfed9ada79a2c5be3b1c10878c6e2807ecb24
Fanfan-cyber <erilanfun@gmail.com>
2025-07-23 14:10:40 +0800

    GRAVITYWELL fix

--- Plugins/Tectonic Battle/Abilities/Ability Event Handlers/On Hit/TargetAbilitiesOnHit.rb
+++ Plugins/Tectonic Battle/Abilities/Ability Event Handlers/On Hit/TargetAbilitiesOnHit.rb
@@ -50,12 +50,12 @@ BattleHandlers::TargetAbilityOnHit.add(:LUNARIOT,
 BattleHandlers::TargetAbilityOnHit.add(:GRAVITYWELL,
     proc { |ability, user, target, move, battle, aiCheck, aiNumHits|
         if aiCheck
-            next getGravityEffectScore(battler, 4)
+            next getGravityEffectScore(user, 4)
         else
-            battle.pbShowAbilitySplash(battler, ability)
-            battle.pbAnimation(:GRAVITY, battler, nil, 0)
+            battle.pbShowAbilitySplash(target, ability)
+            battle.pbAnimation(:GRAVITY, target, nil, 0)
             battle.field.applyEffect(:Gravity, 4)
-            battle.pbHideAbilitySplash(battler)
+            battle.pbHideAbilitySplash(target)
         end
     }
 )
