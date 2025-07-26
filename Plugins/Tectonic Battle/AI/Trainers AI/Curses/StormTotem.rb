d5ec2264e128b918366c493d025ec83123638d6c
ShadowKirbae <toni1009.tn@gmail.com>
2025-07-26 19:43:35 +0200

    Adding the 4 Curses for the Tapu fights

---
+++ Plugins/Tectonic Battle/AI/Trainers AI/Curses/StormTotem.rb
@@ -0,0 +1,17 @@
+PokeBattle_Battle::BattleStartApplyCurse.add(:CURSE_STORM_TOTEM,
+    proc { |curse_policy, battle, curses_array|
+        battle.amuletActivates(
+            _INTL("TODO"),
+            _INTL("Enemy Pokémon experience the constant effects of Storm Totem."),
+        )
+        curses_array.push(curse_policy)
+        next curses_array
+    }
+)
+
+PokeBattle_Battle::BattlerEnterCurseEffect.add(:CURSE_STORM_TOTEM,
+    proc { |_curse_policy, battler, _battle|
+        next unless battler.opposes?
+        battler.pbOwnSide.applyEffect(:TurbulentSky, 1000)
+    }
+)
