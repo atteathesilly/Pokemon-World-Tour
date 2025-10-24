49aa5530ba5988784f2705ff0f7c31cf46a513c3
jessicagaylord <thetotalmaster@gmail.com>
2025-10-24 11:55:39 -0500

    DARKRAI avatar tweak
    removed LULLABY as it was too exploitable
    added sleep check to COTV and removed turn counter in order to make the avatar use it more intelligently

--- Plugins/Tectonic Battle/AI/AI_Boss_Classes.rb
+++ Plugins/Tectonic Battle/AI/AI_Boss_Classes.rb
@@ -407,14 +407,18 @@ class PokeBattle_AI_DARKRAI < PokeBattle_AI_Boss
     def initialize(user, battle)
         super
         @warnedIFFMove.add(:CALLOFTHEVOID, {
-            :condition => proc { |_move, user, _target, battle|
-                next battle.turnCount % 3 == 0
+            :condition => proc { |_move, user, _target, _battle|
+                anyAsleep = false
+                user.battle.battlers.each do |b|
+                    next if !b || !user.opposes?(b)
+                    anyAsleep = true if b.asleep?
+                end
+                next !anyAsleep
             },
             :warning => proc { |_move, user, _targets, _battle|
                 _INTL("The air around {1} turns dark and distorted.",user.pbThis(true))
             },
         })
-        @requiredMoves.push(:LULLABY)
     end
 end
 
