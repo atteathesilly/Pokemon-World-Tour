80c8bd7064728bd809ba03425aeb0ccfd9ebe04c
Fanfan-cyber <erilanfun@gmail.com>
2025-11-16 13:00:27 +0800

    comment out attr_accessor :items
    monkey patch WARNING: items monkey patched in Pokemon
    check line 707 for the getter
    I doubt that if we need the setter

--- Plugins/Tectonic Battle/Pokemon/Pokemon.rb
+++ Plugins/Tectonic Battle/Pokemon/Pokemon.rb
@@ -33,7 +33,7 @@ class Pokemon
     # @return [Array<Integer>] the IDs of moves known by this Pokémon when it was obtained
     attr_accessor :first_moves
     # @return [Array<Pokemon::Item>] the items held by this Pokémon
-    attr_accessor :items
+    # attr_accessor :items
     # @return [Array<Symbol>] an array of ribbons owned by this Pokémon
     attr_accessor :ribbons
     # @return [Integer] contest stats
