d2ef5b91b0d081082bbcae9708ac8020a3b4e9bc
ShadowKirbae <toni1009.tn@gmail.com>
2025-11-21 22:27:11 +0100

    Denticle Debris ability popup fix

--- Plugins/Tectonic Battle/Abilities/Ability Event Handlers/Move End/UserAbilityEndOfTrappingMove.rb
+++ Plugins/Tectonic Battle/Abilities/Ability Event Handlers/Move End/UserAbilityEndOfTrappingMove.rb
@@ -2,7 +2,11 @@ BattleHandlers::UserAbilityEndOfTrappingMove.add(:DENTICLEDEBRIS,
     proc { |ability, user, target, move, battle|
         battle.pbShowAbilitySplash(user, ability)
         side = user.pbOpposingSide
-        next if side.effectAtMax?(:Spikes)
+        if side.effectAtMax?(:Spikes)
+            battle.pbDisplay(_INTL("But it failed, since there is no room for more Spikes!"))
+            battle.pbHideAbilitySplash(user)
+        next
+        end
         side.incrementEffect(:Spikes)
         battle.pbDisplay(_INTL("{1} scattered debris that became Spikes!", user.pbThis))
         battle.pbHideAbilitySplash(user)
