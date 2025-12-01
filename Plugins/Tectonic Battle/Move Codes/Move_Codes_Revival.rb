781aee9e5d7f44d1fe1923892eba9faf5c671bc7
ZinniaGameDev <xeuorux@gmail.com>
2025-11-30 19:54:25 -0700

    Tweak the math on RevivePartyMemberTo50HPThenSwitch's healing
    Always round up, to avoid weird issues with mons with 1 HP (Shedinja)
    And use totalhp instead of hp for clarity

--- Plugins/Tectonic Battle/Move Codes/Move_Codes_Revival.rb
+++ Plugins/Tectonic Battle/Move Codes/Move_Codes_Revival.rb
@@ -69,7 +69,7 @@ class PokeBattle_Move_RevivePartyMemberTo50HPThenSwitch < PokeBattle_Move
 
     def effectOnPartyMember(pokemon)
         pokemon.heal
-        pokemon.hp = (0.5*pokemon.hp).round
+        pokemon.hp = (pokemon.totalhp / 2.0).ceil
     end
 
     def pbEffectGeneral(user)
