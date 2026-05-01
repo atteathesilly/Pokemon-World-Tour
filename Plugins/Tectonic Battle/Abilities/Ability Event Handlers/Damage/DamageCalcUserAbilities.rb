0190e865802f4d100b33f8667f8674528b3033d4
atteathesilly <rathfan69@gmail.com>
2026-04-30 21:02:42 -0400

    damage calc user ability for furret

--- Plugins/Tectonic Battle/Abilities/Ability Event Handlers/Damage/DamageCalcUserAbilities.rb
+++ Plugins/Tectonic Battle/Abilities/Ability Event Handlers/Damage/DamageCalcUserAbilities.rb
@@ -433,6 +433,15 @@ BattleHandlers::DamageCalcUserAbility.add(:STEELYSPIRIT,
   }
 )
 
+BattleHandlers::DamageCalcUserAbility.add(:ALLYOFBUGS,
+  proc { |ability, user, target, move, mults, _baseDmg, type, aiCheck|
+    if type == :BUG
+      mults[:base_damage_multiplier] *= 1.5
+      user.aiLearnsAbility(ability) unless aiCheck
+    end
+  }
+)
+
 BattleHandlers::DamageCalcUserAbility.add(:VERDANT,
   proc { |ability, user, target, move, mults, _baseDmg, type, aiCheck|
     if type == :GRASS
