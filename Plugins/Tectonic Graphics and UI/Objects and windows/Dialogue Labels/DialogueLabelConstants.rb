a5766e5b2caf032939bb40e5f44277161137d77e
atteathesilly <rathfan69@gmail.com>
2026-09-13 00:42:27 -0400

    adjusting cr sirfceted

--- Plugins/Tectonic Graphics and UI/Objects and windows/Dialogue Labels/DialogueLabelConstants.rb
+++ Plugins/Tectonic Graphics and UI/Objects and windows/Dialogue Labels/DialogueLabelConstants.rb
@@ -48,7 +48,7 @@ HARMONY = _INTL("Harmony")
 HANNAH = _INTL("Hannah")
 CLOWN = _INTL("Clown")
 JAN = _INTL("Jan")
-SYDNEY = _INTL("captain Sydney")
+SYDNEY = _INTL("Captain Sydney")
 GRISHAM = _INTL("Grisham")
 GRISELLE = _INTL("Griselle")
 
